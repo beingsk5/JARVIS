3511a7506b3af329be3b080f6668f86d58fa7bbe
beingsk5 <takeiteasy4possible@gmail.com>
2025-04-22 18:23:24 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,35 +13,40 @@ fi
 cd kernel
 
 # Export required variables
+export KERNEL_DEFCONFIG="gulch_defconfig"
+export KERNEL_CMDLINE=
+"ARCH=arm64
+CC=clang
+CROSS_COMPILE_COMPAT=aarch64-linux-android-
+CROSS_COMPILE=aarch64-linux-gnu-
+CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+CLANG_TRIPLE=aarch64-linux-gnu-
+READELF=llvm-readelf
+LLVM_DIS=llvm-dis
+AR=llvm-ar
+NM=llvm-nm
+OBJCOPY=llvm-objcopy
+OBJDUMP=llvm-objdump
+STRIP=llvm-strip
+LLVM=1
+LLVM_IAS=1
+LD=ld.lld
+HOSTCC=clang
+HOSTCXX=clang++
+O=out"
 export PATH=$(pwd)/toolchains/neutron-clang/bin/:$PATH
 export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_COMPILER_STRING=$(clang --version | head -n 1)
-export CCACHE_EXEC=$(which ccache)
 export KBUILD_BUILD_HOST="@beingsk5"
-export LLVM=1
-export LLVM_IAS=1
-echo "CONFIG_BUILD_ARM64_DT_OVERLAY=y" >> gulch_defconfig
+export BRAND_SHOW_FLAG=oneplus
+export TARGET_PRODUCT=msmnile
+# echo "CONFIG_BUILD_ARM64_DT_OVERLAY=y" >> gulch_defconfig
 
 # Configure kernel     
-make O=out ARCH=arm64 gulch_defconfig 
+cd kernel
+make $KERNEL_CMDLINE CC="ccache clang" $KERNEL_DEFCONFIG
 # | make O=out ARCH=arm64 olddefconfig
 
 # Build kernel
-make -j$(nproc --all) O=out \
-    ARCH=arm64 \
-    CC="ccache clang" \
-    HOSTCC=clang \
-    HOSTCXX=clang++ \
-    LD=ld.lld \
-    AR=llvm-ar \
-    NM=llvm-nm \
-    LLVM_IAS=1 \
-    LLVM=1 \
-    STRIP=llvm-strip \
-    OBJCOPY=llvm-objcopy \
-    OBJDUMP=llvm-objdump \
-    CLANG_TRIPLE=aarch64-linux-gnu- \
-    CROSS_COMPILE=aarch64-linux-android- \
-    CROSS_COMPILE_ARM32=arm-linux-androideabi- \
-    CROSS_COMPILE_COMPAT=arm-linux-androidkernel-
+make $KERNEL_CMDLINE CC="ccache clang" -j$(nproc --all)
